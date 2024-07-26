13979a94b2407bc940c930f172c63ddd17902b5f
chanhee7 <chan9505@naver.com>
2024-07-26 11:33:07 +0900

    # 인증 액션 디스패치

--- src/redux-practice/store/index.js
+++ src/redux-practice/store/index.js
@@ -49,7 +49,14 @@ const initialAuthState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState: initialAuthState,
-  reducers: {}
+  reducers: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    }
+  }
 });
 
 // 단 하나의 리덕스 스토어
@@ -63,6 +70,7 @@ const store = configureStore({
 
 // 슬라이스 안에 reducers에 정의한 함수들을 내보내기
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 // 리액트의 index.js에게 store를 제공
 export default store;
