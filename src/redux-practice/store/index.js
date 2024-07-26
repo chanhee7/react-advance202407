533b1ed0c5e9b744cfda8b695192744e97286002
chanhee7 <chan9505@naver.com>
2024-07-26 10:16:28 +0900

    # 리듀서 액션 동적으로 제어

--- src/redux-practice/store/index.js
+++ src/redux-practice/store/index.js
@@ -4,6 +4,7 @@ import { createStore } from "redux";
 // 액션 타입 상수
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
+export const DOUBLE = 'DOUBLE';
 
 // 관리할 초기 상태값 객체
 const initialCountState = {
@@ -27,11 +28,15 @@ const counterReducer = (state = initialCountState, action) => {
     case INCREMENT:
       return {
         counter: state.counter + 1
-      }
+      };
     case DECREMENT:
       return {
         counter: state.counter - 1
-      }
+      };
+    case DOUBLE:
+      return {
+        counter: state.counter * action.payload
+      };
     default:
       return state;
   }
